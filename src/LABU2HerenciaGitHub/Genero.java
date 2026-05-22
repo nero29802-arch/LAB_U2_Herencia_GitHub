322543f857b75662af1eac93fadcb31f99e0048b
Personal <Personal@DESKTOP-I3UK99Q>
2026-05-21 20:14:43 -0600

    Unidad 3 Trabajo BreynerMora - Tesis,Genero,AudioLibro

---
+++ src/LABU2HerenciaGitHub/Genero.java
@@ -0,0 +1,24 @@
+
+package LABU2HerenciaGitHub;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public enum Genero {
+
+    Programacion,
+    BASES_DATOS, 
+    REDES, 
+    INVESTIGACION, 
+    GENERAL,
+    NOVELA,
+    CIENCIA,
+    HISTORIA,
+    TECNOLOGIA,
+    EDUCACION,
+    FANTASIA
+  
+}
