6fe4dd06010d1940ddfbfd42692d05e4547f4c4c
Personal <Personal@DESKTOP-I3UK99Q>
2026-05-22 19:32:08 -0600

    Develop - Unidad 3 - Breyner

--- src/LABU2HerenciaGitHub/Genero.java
+++ src/LABU2HerenciaGitHub/Genero.java
@@ -7,8 +7,11 @@ package LABU2HerenciaGitHub;
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
+
+//creacion de una clase enum 
 public enum Genero {
 
+    //tipos de generos que contiene para libros
     Programacion,
     BASES_DATOS, 
     REDES, 
